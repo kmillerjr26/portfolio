836e30626a0f8aa3cf19ce53ae80314d79d0dc09
kmillerjr26 <mi2k3rb@gmail.com>
2023-10-16 12:17:53 -0400

    commit

---
+++ js/template.js
@@ -0,0 +1,19 @@
+var width = $(document).width();
+
+function changeHamburger() {
+   /* if (width < 768){
+        document.classList.remove("list");
+        document.classList.add("hamburger");
+        $(".body").css("background-color", "blue");
+    }
+    if(width >= 768){
+        $(".hamburger").remove();
+        $(".list").add();
+    }*/
+}
+
+changeHamburger();
+
+$(".hamburger").click(function(){
+    
+});
