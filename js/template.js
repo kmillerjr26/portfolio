2892756454bc5022bc5b471bf9d6782924b67415
kmillerjr26 <mi2k3rb@gmail.com>
2023-10-16 23:28:17 -0400

    commit

--- js/template.js
+++ js/template.js
@@ -1,19 +1,19 @@
 var width = $(document).width();
-
-function changeHamburger() {
-   /* if (width < 768){
-        document.classList.remove("list");
-        document.classList.add("hamburger");
-        $(".body").css("background-color", "blue");
-    }
-    if(width >= 768){
-        $(".hamburger").remove();
-        $(".list").add();
-    }*/
-}
-
-changeHamburger();
+var hamburgerClicked = false;
 
 $(".hamburger").click(function(){
-    
+    /*const element = document.querySelector('.list2')
+    const style = getComputedStyle(element);*/
+    if(width < 768){
+        if(hamburgerClicked == false){
+            $(".list2").css("opacity", "1");
+            $(".list2").css("z-index", "1");
+            hamburgerClicked = true;
+        }
+        else{
+            $(".list2").css("opacity", "0");
+            $(".list2").css("z-index", "-1");
+            hamburgerClicked = false;
+        }
+    }
 });
